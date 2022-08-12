e2c53c8143af9f0de073522005f6a926f4ac0a0e
Damzxyno <decagon@Decagons-MacBook-Air.local>
2022-08-12 17:06:24 +0100

    Created a cqrs project using MediatR

---
+++ ZeroToHero/Controllers/WeatherForecastController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ZeroToHero.Handlers.Queries;
+
+namespace ZeroToHero.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        //public readonly IMediator _mediator;
+        private IMediator _mediator;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IMediator mediator)
+        {
+            _logger = logger;
+            _mediator = mediator;
+            //_mediator = HttpContext.RequestServices.GetService<IMediator>();
+        }
+
+        [HttpGet("bwest")]
+        public IActionResult Get()
+        {
+            _logger.LogInformation("I don arrive");
+            var rng = new Random();
+            return Ok(Enumerable.Range(2, 6).Select(index => new 
+            {
+                Index = index,
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray());
+        }
+
+        [HttpGet("Book")]
+        public IActionResult GetBooks()
+        {
+
+            var query = new BookQuery();
+            var result = _mediator.Send(query);
+            return Ok(result);
+        }
+    }
+}
---
+++ ZeroToHero/Handlers/Queries/BookQuery.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace ZeroToHero.Handlers.Queries
+{
+    public class Book
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class BookQuery : IRequest<List<Book>>
+    {
+        public string Name { get; set; }
+    }
+
+    public class BookQueryHandler : IRequestHandler<BookQuery, List<Book>>
+    {
+        public Task<List<Book>> Handle(BookQuery request, CancellationToken cancellationToken)
+        {
+            List<Book> list = new List<Book>();
+            list.Add(new Book { ID = 1, Name = "Eze goes to school" });
+            list.Add(new Book { ID = 2, Name = "Mustapha is hungry" });
+            list.Add(new Book { ID = 3, Name = "Na IT dey sell now o!" });
+
+            var tcs = new TaskCompletionSource<List<Book>>();
+            tcs.SetResult(list);
+            //return tcs.Task;
+            return Task.FromResult(list);
+        }
+    }
+}
